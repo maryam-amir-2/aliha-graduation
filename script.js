79ca72a2d08c53c20e52f8c842920770185d7dbc
maryam-amir-2 <maryam.amir@bigacademy.com>
2025-07-15 04:06:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const revealBtn = document.getElementById('reveal-gift-btn');
-  const giftWrapper = document.querySelector('.gift-image-wrapper');
+  const revealBtn = document.getElementById('reveal-badge-btn');
+  const badgeWrapper = document.querySelector('.badge-image-wrapper');
 
   revealBtn.addEventListener('click', () => {
-    giftWrapper.classList.add('is-revealed');
+    badgeWrapper.classList.add('is-revealed');
     revealBtn.textContent = 'Gift Revealed!';
     revealBtn.disabled = true; // Prevents multiple clicks
   });
